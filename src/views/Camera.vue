a5669627f3ea7d867fdb88d1d2738be4fdc12a7d
藍奕濡 <a78945612385238@gmail.com>
2021-04-15 18:57:43 +0800

    delete ssd model

--- src/views/Camera.vue
+++ src/views/Camera.vue
@@ -104,7 +104,7 @@ export default {
       await Promise.all([
         faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
         faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-        faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+        // faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
         faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
       ]);
       this.modelStatus = 'loaded';
